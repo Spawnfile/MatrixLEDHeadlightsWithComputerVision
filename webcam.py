0ba504f8349aac20ae757bc41d57f2b0c291421c
Alper Ekmekci <alper.ekmekci@hotmail.com>
2019-12-06 23:47:56 +0300

    Detection coordinates added to webcam.py

--- webcam.py
+++ webcam.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python
 
+"""
+Print functions used for testing and debugging.
+
+These functions will be removed after testing.
+"""
 import os
 import argparse
 import json
@@ -43,11 +48,11 @@ def main():
     print("HEIGHT :", cam_height)
     
     """
-    pc kamerasi icin: 
+    PC Cam Resolution: 
     width : 640
     heigt : 480
 
-    webcam için:
+    External Cam Resolution
     width : 1280
     height : 720
     """
@@ -60,25 +65,37 @@ def main():
             batch_boxes = get_yolo_boxes(infer_model, images, net_h, net_w, config['model']['anchors'], obj_thresh, nms_thresh)
 
             for i in range(len(images)):
-                images[i], bbox = draw_boxes(images[i], batch_boxes[i], config['model']['labels'], obj_thresh) 
-                #bbox = ...
-                # bu kısımda pixel kordinatlarının orta noktaları hesaplanacak 
+                images[i], bbox, xmax, ymax = draw_boxes(images[i], batch_boxes[i], config['model']['labels'], obj_thresh)  
+                if xmax != None:
+                    print("Detection True First Kontrol")
+                    print("XMAX :", xmax[0])
+                    print("YMAX :", ymax[0])
+                    print("XMIN : ", bbox[0][0])
+                    print("Mid Point : ", (xmax[0]+(bbox[0][0] + 3)) / 2)
+                    mid_point = math.ceil((xmax[0]+(bbox[0][0] + 3)) / 2)
+                    mid_point_line_1 = (mid_point, ymax + 10)
+                    mid_point_line_2 = (mid_point, ymax - 30) 
+                    cv2.line(images[i], mid_point_line_1, mid_point_line_2, (0, 255, 0))
+                """
+                except:
+                    print("Detection False First Control")
+                """
                 cv2.line(images[i], line_1_1, line_1_2, (255, 0, 0))
                 cv2.line(images[i], line_2_1, line_2_2, (255, 0, 0))
-                cv2.imshow('video with bboxes', images[i])
+                cv2.namedWindow("Video Stream", cv2.WND_PROP_FULLSCREEN)
+                cv2.setWindowProperty("Video Stream", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+                cv2.imshow('Video Stream', images[i])
 
                 try:
+                    #DETECTION IS TRUE BLOCK 
                     if bbox.any() != None:
-                        #detection varsa
                         print("Detection is True")
-                        #do something
-                        #detection yok
-                        #print("Detection is False")
-                        #do something
+
+                    #DETECTION IS FALSE BLOCK
                 except:
                     print("detection yok")
-                    pass
-                
+                    print(bbox)
+
                 print('FPS {:.1f}'.format(1 / (time.time() - stime))) 
             images = []
             print("ikinci resim")
